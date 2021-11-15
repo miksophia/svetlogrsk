bcf8ca4d6defb23142f2a9f62e5247d6609ed138
Евгения <69339596+EvgeshaJA@users.noreply.github.com>
2021-11-15 11:07:12 +0300

    Add files via upload

--- source/js/modules/calling.js
+++ source/js/modules/calling.js
@@ -2,25 +2,26 @@ export function calling(){
 
     const buttonCalling = document.querySelector('.payment-method__rate');
     const buttonCheck = document.querySelectorAll('.table-calling')
-    const hiddenBlock = document.querySelector('.organization-choise');
+    const hiddenBlock = document.querySelector('.chapter-choise');
     const arrowUp = document.querySelector('.arow-down');
     
 
     buttonCalling.onclick = function(){
-      hiddenBlock.classList.toggle("organization-choise--active");
+      hiddenBlock.classList.toggle("chapter-choise--active");
       arrowUp.classList.toggle("arow-down--active");
     };
 
     buttonCheck.forEach((item) => {
       item.addEventListener('click', () => {        
 
-      if (hiddenBlock.classList.contains("organization-choise--active")) {
-          hiddenBlock.classList.remove("organization-choise--active");
+      if (hiddenBlock.classList.contains("chapter-choise--active")) {
+          hiddenBlock.classList.remove("chapter-choise--active");
 
         } else {
-          hiddenBlock.classList.toggle("organization-choise--active");
+          hiddenBlock.classList.toggle("chapter-choise--active");
         }
       });
   });
+
 
 };
