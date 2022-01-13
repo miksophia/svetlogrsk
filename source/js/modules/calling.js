cd208d22e0d494dcc3717f1f39568de946bba8f0
Sophia <miksofia@mail.ru>
2022-01-13 12:18:11 +0300

    Версия на админке

--- source/js/modules/calling.js
+++ source/js/modules/calling.js
@@ -1,71 +1,75 @@
-export function calling(){
+export function calling() {
+  // ТАБЛИЦА
 
+  const buttonCalling = document.querySelector(".payment-method__rate"); //add
+  const buttonCallingInfo = document.querySelector(
+    ".payment-method__rate--info"
+  ); //add info
 
-      // ТАБЛИЦА
+  const hiddenBlock = document.querySelector(".adv__item__wrap"); //table
+  const arrowUp = document.querySelector(".arow-down"); // arrow
+  const hiddenInfo = document.querySelector(".adv-info"); // info
 
-      const buttonCalling = document.querySelector('.payment-method__rate'); //add
-      const buttonCallingInfo = document.querySelector('.payment-method__rate--info'); //add info
-    
-      const hiddenBlock = document.querySelector('.adv__item__wrap'); //table
-      const arrowUp = document.querySelector('.arow-down'); // arrow
-      const hiddenInfo = document.querySelector('.adv-info');// info
-    
-    if (hiddenBlock) {
-      buttonCalling.addEventListener('click', () => {
-        hiddenBlock.classList.toggle("adv__item__wrap--active");
-        arrowUp.classList.toggle("arow-down--active");
-      });
-    }
-      if (hiddenInfo) {
-        buttonCallingInfo.addEventListener('click', () => {
-          hiddenInfo.classList.toggle("adv-info--active");
-          arrowUp.classList.toggle("arow-down--active");
-        });
+  if (hiddenBlock) {
+    buttonCalling.addEventListener("click", () => {
+      hiddenBlock.classList.toggle("adv__item__wrap--active");
+      arrowUp.classList.toggle("arow-down--active");
+    });
+  }
+  if (hiddenInfo) {
+    buttonCallingInfo.addEventListener("click", () => {
+      hiddenInfo.classList.toggle("adv-info--active");
+      arrowUp.classList.toggle("arow-down--active");
+    });
+  }
+
+  // ДОБАВИТЬ ОБЪЯВЛЕНИЕ
+
+  const commentMax = document.querySelector(".form__card--maxy");
+
+  var massOne = document.querySelectorAll(".block--hidden");
+  var massTwo = document.querySelectorAll(".card-click");
+
+  let removeActive = function (array, activeClass) {
+    array.forEach(function (item) {
+      item.classList.remove(activeClass);
+    });
+    return;
+  };
+  massOne.forEach(function (item, i) {
+    item.addEventListener("click", function (evt) {
+      removeActive(massTwo, "card-click--active");
+      removeActive(massOne, "block--active");
+      commentMax.style.display = "block";
+      if (massOne[i].classList.contains("block--active")) {
+      } else {
+        massOne[i].classList.add("block--active");
+        massTwo[i].classList.add("card-click--active");
       }
-    
-      // ДОБАВИТЬ ОБЪЯВЛЕНИЕ
-    
-      const commentMax = document.querySelector(".form__card--maxy");
+    });
+  });
 
-      var massOne = document.querySelectorAll('.block--hidden');
-      var massTwo = document.querySelectorAll('.card-click');
-
-      let removeActive = function (array, activeClass) {
-        array.forEach(function (item) {
-          item.classList.remove(activeClass);
-        });
-        return;
-      };
-      massOne.forEach(function (item, i) {
-        item.addEventListener("click", function (evt) {
-          removeActive(massTwo, "card-click--active");
-          removeActive(massOne, "block--active");
-          commentMax.style.display = 'block';
-          if (massOne[i].classList.contains("block--active")) {
-          } else {
-            massOne[i].classList.add("block--active");
-            massTwo[i].classList.add("card-click--active");
-          }
-        });
-      });
-
-      const workHiddenBlok = document.querySelector('.work__hidden-blok');
-      const workBlock = document.querySelector('.work');
-
-      if (workBlock.classList.contains("card-click--active")) {
-        workHiddenBlok.style.display = 'block';
-      };
-
-
+  // const workHiddenBlok = document.querySelector('.work__hidden-blok');
+  // const workBlock = document.querySelector('.work');
+  //
+  // if (workBlock.classList.contains("card-click--active")) {
+  //   workHiddenBlok.style.display = 'block';
+  // };
 
   // ADD NUMBER
-
-  var d = document;
-  d.querySelector('.form__add-number').addEventListener('click', function(){
-      var addNumber = d.createElement('div');
-      addNumber.className = 'form__numbers-create';
-      addNumber.innerHTML = '<input class="form__numbers" type="tel" name="asv-number" placeholder="Номер телефона">';
-      d.querySelector('.form__card-addNum').appendChild(addNumber);
-  }, false);
-
-};
+  let pdl = document.querySelector(".form__add-number");
+  if (pdl) {
+    var d = document;
+    d.querySelector(".form__add-number").addEventListener(
+      "click",
+      function () {
+        var addNumber = d.createElement("div");
+        addNumber.className = "form__numbers-create";
+        addNumber.innerHTML =
+          '<input class="form__numbers" type="tel" name="asv-number" placeholder="Номер телефона">';
+        d.querySelector(".form__card-addNum").appendChild(addNumber);
+      },
+      false
+    );
+  }
+}
--- source/js/modules/modals.js
+++ source/js/modules/modals.js
@@ -1,120 +1,158 @@
+export function modals() {
+  // const addItem = document.querySelector(".header__add-item");
+  //
+  // // MODAL
+  //
+  // // add
+  //
+  // const modalAdd = document.querySelector(".add");
+  // const closeModalAdd = document.querySelector(".add__close");
+  //
+  // addItem.onclick = function () {
+  //   modalAdd.classList.add("add--active");
+  //   document.body.style.overflow = "hidden";
+  // };
+  //
+  // modalAdd.addEventListener("click", (e) => {
+  //   e.preventDefault();
+  //   modalAdd.classList.remove("add--active");
+  //   // modalAdd.classList.remove("enter--active");
+  //   document.body.style.overflow = "";
+  // });
 
-export function modals(){
+  const modalTrigger = document.querySelectorAll(".header__add-item"),
+    modal = document.querySelector(".add"),
+    modalCloseBtn = document.querySelector(".add__close");
 
-    const addItem = document.querySelector('.header__add-item');
-
-  // MODAL 
-
-  // add
-
-  const modalAdd = document.querySelector('.add');
-  const closeModalAdd = document.querySelector('.add__close');
-
-    addItem.onclick = function(){
-      modalAdd.classList.add("add--active");
-    };
-
-    closeModalAdd.onclick = function(){
-      modalAdd.classList.remove("add--active");
-      modalAdd.classList.remove("enter--active");
-
-      
-    };
-  
-  // enter
-
-  const enter = document.querySelector('.header__entry');
-
-  const closeModalEnter = document.querySelector('.enter__close');
-  const modalEnter = document.querySelector('.enter');
-
-  if(enter) {
-    enter.addEventListener('click', () => {
-      modalEnter.classList.add("enter--active");
+  if (modal != null) {
+    modalTrigger.forEach((btn) => {
+      btn.addEventListener("click", function () {
+        modal.classList.add("add--active");
+        modal.classList.remove("hide");
+        // Либо вариант с toggle - но тогда назначить класс в верстке
+        document.body.style.overflow = "hidden";
+      });
     });
-  }
 
-  if(closeModalEnter) {
-    closeModalEnter.addEventListener('click', () => {
-      modalEnter.classList.remove("enter--active");
+    function closeModal() {
+      modal.classList.add("hide");
+      modal.classList.remove("add--active");
+      // Либо вариант с toggle - но тогда назначить класс в верстке
+      document.body.style.overflow = "";
+    }
+
+    modalCloseBtn.addEventListener("click", () => {
+      closeModal();
     });
-  }
 
-  // registration
-
-  const newUser = document.querySelector('.modal__link-registration');
-  const registration = document.querySelector('.registration');
-  const closeRegistration = document.querySelector('.registration__close');
-  const closeRegistrationButton = document.querySelector('.modal__button-cancel');
-
-  if(newUser) {
-    newUser.addEventListener('click', () => {
-      registration.classList.add('registration--active');
-      modalEnter.classList.remove("enter--active");
+    modal.addEventListener("click", (e) => {
+      if (e.target === modal) {
+        closeModal();
+      }
     });
-  }
-  
 
-  if(closeRegistration) {
-    closeRegistration.addEventListener('click', () => {
-      registration.classList.remove('registration--active');
+    document.addEventListener("keydown", (e) => {
+      if (e.code === "Escape" && modal.classList.contains("add--active")) {
+        closeModal();
+      }
     });
+
+    // enter
+
+    const enter = document.querySelector(".header__entry");
+
+    const closeModalEnter = document.querySelector(".enter__close");
+    const modalEnter = document.querySelector(".enter");
+
+    if (enter) {
+      enter.addEventListener("click", () => {
+        modalEnter.classList.add("enter--active");
+        document.body.style.overflow = "hidden";
+      });
+    }
+
+    if (closeModalEnter) {
+      closeModalEnter.addEventListener("click", () => {
+        modalEnter.classList.remove("enter--active");
+        document.body.style.overflow = "";
+      });
+    }
+
+    // registration
+
+    const newUser = document.querySelector(".modal__link-registration");
+    const registration = document.querySelector(".registration");
+    const closeRegistration = document.querySelector(".registration__close");
+    const closeRegistrationButton = document.querySelector(
+      ".modal__button-cancel"
+    );
+
+    if (newUser) {
+      newUser.addEventListener("click", () => {
+        registration.classList.add("registration--active");
+        modalEnter.classList.remove("enter--active");
+      });
+    }
+
+    if (closeRegistration) {
+      closeRegistration.addEventListener("click", () => {
+        registration.classList.remove("registration--active");
+        document.body.style.overflow = "";
+      });
+    }
+
+    if (closeRegistrationButton) {
+      closeRegistrationButton.addEventListener("click", () => {
+        registration.classList.remove("registration--active");
+        document.body.style.overflow = "";
+      });
+    }
+
+    // recovery
+
+    const recoveryPass = document.querySelector(".modal__link-fotgot");
+    const recoveryModal = document.querySelector(".recovery");
+    const recoveryClose = document.querySelector(".recovery__close");
+
+    if (recoveryPass) {
+      recoveryPass.addEventListener("click", () => {
+        recoveryModal.classList.add("recovery--active");
+        modalEnter.classList.remove("enter--active");
+      });
+    }
+
+    if (recoveryClose) {
+      recoveryClose.addEventListener("click", () => {
+        recoveryModal.classList.remove("recovery--active");
+        document.body.style.overflow = "";
+      });
+    }
   }
-
-  if(closeRegistrationButton) {
-    closeRegistrationButton.addEventListener('click', () => {
-      registration.classList.remove("registration--active");
-    });
-  }
-  
-  // recovery
-
-  const recoveryPass = document.querySelector('.modal__link-fotgot');
-  const recoveryModal = document.querySelector('.recovery');
-  const recoveryClose = document.querySelector('.recovery__close');
-
-  if(recoveryPass) {
-    recoveryPass.addEventListener('click', () => {
-      recoveryModal.classList.add('recovery--active');
-      modalEnter.classList.remove("enter--active");
-    });
-  }
-
-  if(recoveryClose) {
-    recoveryClose.addEventListener('click', () => {
-      recoveryModal.classList.remove('recovery--active');
-    });
-  }
-
-
   // УДАЛЕНИЕ ОБЪЯВЛЕНИЙ, ВАКАНСИЙ, УСЛУГ И Т.Д.
 
-  const deleteAdv = document.querySelector('.delete__adv');
-  const deleteAdvButton = document.querySelector('.delete__adv-button');
-  const deleteAdvClose = document.querySelector('.delete__adv-cancel');
+  const deleteAdv = document.querySelector(".delete__adv");
+  const deleteAdvButton = document.querySelector(".delete__adv-button");
+  const deleteAdvClose = document.querySelector(".delete__adv-cancel");
 
-  const deletedAdv = document.querySelector('.deleted__adv');
-  const deletedClose = document.querySelector('.deleted__adv__close')
+  const deletedAdv = document.querySelector(".deleted__adv");
+  const deletedClose = document.querySelector(".deleted__adv__close");
 
-  
-  if(deleteAdvButton) {
-    deleteAdvButton.addEventListener('click', () => {
+  if (deleteAdvButton) {
+    deleteAdvButton.addEventListener("click", () => {
       deleteAdv.classList.remove("delete__adv--active");
       deletedAdv.classList.add("deleted__adv--active");
     });
   }
-  
 
-  if(deleteAdvClose) {
-    deleteAdvClose.addEventListener('click', () => {
+  if (deleteAdvClose) {
+    deleteAdvClose.addEventListener("click", () => {
       deleteAdv.classList.remove("delete__adv--active");
     });
   }
 
-  if(deletedClose) {
-    deletedClose.addEventListener('click', () => {
+  if (deletedClose) {
+    deletedClose.addEventListener("click", () => {
       deletedAdv.classList.remove("deleted__adv--active");
     });
   }
-
 }
